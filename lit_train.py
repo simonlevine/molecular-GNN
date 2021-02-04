b760760b37a887a300188158ca3769f7786eb139
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2021-02-03 20:28:24 -0800

    Update lit_train.py

--- lit_train.py
+++ lit_train.py
@@ -38,6 +38,64 @@ from torch_geometric.nn import BatchNorm, global_mean_pool
 
 from models.pytorch_geometric.pna import PNAConvSimple
 
+try:
+    from rdkit import Chem
+except ImportError:
+    Chem = None
+
+x_map = {
+    'atomic_num':
+    list(range(0, 119)),
+    'chirality': [
+        'CHI_UNSPECIFIED',
+        'CHI_TETRAHEDRAL_CW',
+        'CHI_TETRAHEDRAL_CCW',
+        'CHI_OTHER',
+    ],
+    'degree':
+    list(range(0, 11)),
+    'formal_charge':
+    list(range(-5, 7)),
+    'num_hs':
+    list(range(0, 9)),
+    'num_radical_electrons':
+    list(range(0, 5)),
+    'hybridization': [
+        'UNSPECIFIED',
+        'S',
+        'SP',
+        'SP2',
+        'SP3',
+        'SP3D',
+        'SP3D2',
+        'OTHER',
+    ],
+    'is_aromatic': [False, True],
+    'is_in_ring': [False, True],
+}
+
+e_map = {
+    'bond_type': [
+        'misc',
+        'SINGLE',
+        'DOUBLE',
+        'TRIPLE',
+        'AROMATIC',
+    ],
+    'stereo': [
+        'STEREONONE',
+        'STEREOZ',
+        'STEREOE',
+        'STEREOCIS',
+        'STEREOTRANS',
+        'STEREOANY',
+    ],
+    'is_conjugated': [False, True],
+}
+
+
+
+
 class LitClassifier(pl.LightningModule):
     def __init__(self, net, learning_rate=1e-3):
         super().__init__()
