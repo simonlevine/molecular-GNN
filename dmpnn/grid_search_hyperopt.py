73c624270448f855a80cfe030b838d12d63c6a67
Simon Levine-Gottreich <50503513+slevineg@users.noreply.github.com>
2021-02-03 18:20:17 -0800

    rm scripts

--- dmpnn/grid_search_hyperopt.py
+++
@@ -1,4 +0,0 @@
-from chemprop.grid_search_hyperopt import chemprop_grid_search
-
-if __name__ == '__main__':
-    chemprop_grid_search()
--- dmpnn/predict.py
+++
@@ -1,8 +0,0 @@
-"""Loads a trained chemprop model checkpoint and makes predictions on a dataset."""
-
-from chemprop.train import chemprop_predict
-from loguru import logger
-
-if __name__ == '__main__':
-    logger.warning('Commencing prediction!')
-    chemprop_predict()
